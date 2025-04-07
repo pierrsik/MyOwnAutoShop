cbb4d2f649df5f866c5b7694df1bbe6dbd9654f9
pierrsik <pierrskanikai@gmail.com>
2025-04-07 18:16:40 +0530

    added car interface skeleton

--- Car.java
+++ Car.java
@@ -1,3 +1,28 @@
 public interface Car{
-    
+    // Data members (implicitly public, static, and final)
+    int speed = 0;
+    double price = 0.0;
+    String color = "";
+    String type = "";
+    double mileage = 0.0;
+    String fuelType = "";
+
+    // Abstract getters 
+    int getSpeed();
+    double getPrice();
+    String getColor();
+    String getType();
+    double getMileage();
+    String getFuelType();
+
+    // abstract setters;
+    void setSpeed();
+    void setPrice();
+    void setColor();
+    void setType();
+    void setMileage();
+    void setFuelType();
+
+    //abstarct getSalePrice() method
+    double getSalePrice();
 }
