c5cd49748e2bf876657fbb2127472fd9151b7625
Hyperstrom <aniketpal28112003@gmail.com>
2025-04-09 17:43:46 +0530

    Fix issues in validation class

--- Validation.java
+++ Validation.java
@@ -5,149 +5,149 @@ public class Validation {
 
     private static ArrayList<String> colorList = new ArrayList<>(Arrays.asList("red", "blue", "green"));
 
-    public boolean validateSpeed(int speed){
-        try{
-            if(speed<0){
-                System.out.println("Speed cant be negative");
+    public boolean validateSpeed(int speed) {
+        try {
+            if (speed < 0) {
+                System.out.println("Speed can not be negative");
                 return false;
-            }
-            else if(speed>250){
+            } else if (speed > 250) {
                 System.out.println("Speed must be less than 250");
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
-        }
-        catch(Exception e){
-            System.out.println("Check your input for invalid values");
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
     }
 
-
-    public boolean validatePrice(double price){
-        double minPrice=0.0;
-        double maxPrice=1000000.0;
-        try{
-            if(price<minPrice){
+    public boolean validatePrice(double price) {
+        double minPrice = 0.0;
+        double maxPrice = 100000000.0;
+        try {
+            if (price < minPrice) {
                 System.out.println("Price must not be negative");
                 return false;
-            }
-            else if(price>maxPrice){
+            } else if (price > maxPrice) {
                 System.out.println("Price must be reasonable");
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
-        }
-        catch(Exception e){
-            System.out.println("Check your input for invalid values");
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
     }
 
-    public boolean validateColor(String color){
-        color = color.toLowerCase();
-        if(colorList.contains(color)){
-            return true;
-        }
-        else{
-            System.out.println("That color option is not offerred");
+    public boolean validateColor(String color) {
+        try {
+            color = color.toLowerCase();
+            if (colorList.contains(color)) {
+                return true;
+            } else {
+                System.out.println("That color option is not offered");
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
+
     }
 
-    public boolean validateType(String type){
-        type = type.toLowerCase();
-        if(!type.equals("suv")  && !type.equals("sedan")){
-            System.out.println("That color option is not offerred");
-            return true;
-        }
-        else{
+    public boolean validateType(String type) {
+        try {
+            type = type.toLowerCase();
+            if (!type.equals("suv") && !type.equals("sedan")) {
+                System.out.println("That type of Car option is not offered");
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
+
     }
 
-    public boolean validateMileage(double mileage){
-        double minMileage=0.0;
-        double maxMileage=30.0;
-        try{
-            if(mileage<minMileage){
+    public boolean validateMileage(double mileage) {
+        double minMileage = 0.0;
+        double maxMileage = 60.0;
+        try {
+            if (mileage < minMileage) {
                 System.out.println("Mileage must not be negative");
                 return false;
-            }
-            else if(mileage>maxMileage){
+            } else if (mileage > maxMileage) {
                 System.out.println("Mileage must be reasonable");
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
-        }
-        catch(Exception e){
-            System.out.println("Check your input for invalid values");
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
     }
-    
-    public boolean validateFuelType(String fuelType){
-        fuelType = fuelType.toLowerCase();
-        if(!fuelType.equals("petrol")  && !fuelType.equals("diesel")){
-            System.out.println("That fuel vehicle is not invented yet!!");
-            return true;
-        }
-        else{
+
+    public boolean validateFuelType(String fuelType) {
+        try {
+            fuelType = fuelType.toLowerCase();
+            if (!fuelType.equals("petrol") && !fuelType.equals("diesel")) {
+                System.out.println("That fuel vehicle is not invented yet!!");
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
+
     }
 
     public static boolean validateYear(String year) {
-        // Check if the year is a 4-digit number
-        try{
+
+        try {
+            // Check if the year is a 4-digit number
             if (year.matches("\\d{4}")) {
                 int yearInt = Integer.parseInt(year);
                 // Check if the year is within a reasonable range (e.g., 1000 to 9999)
-                if(yearInt >= 1950 && yearInt <= 2400) return true;
-                else{
+                if (yearInt >= 1950 && yearInt <= 2025)
+                    return true;
+                else {
                     System.out.println("Please enter reasonable year value");
                     return false;
                 }
-            }
-            else{
+            } else {
                 System.out.println("Enter valid input");
                 return false;
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Enter numeric value for year");
             return false;
         }
     }
-    
-    public boolean validateManafacturerDiscount(double manafacturerDiscount){
-        double minManafacturerDiscount=0.0;
-        double maxManafacturerDiscount=20.0;
-        try{
-            if(manafacturerDiscount<minManafacturerDiscount){
-                System.out.println("Mileage must not be negative");
+
+    public boolean validateManafacturerDiscount(double manafacturerDiscount) {
+        double minManafacturerDiscount = 0.0;
+        double maxManafacturerDiscount = 20.0;
+        try {
+            if (manafacturerDiscount < minManafacturerDiscount) {
+                System.out.println("Discount can not be negative");
                 return false;
-            }
-            else if(manafacturerDiscount>maxManafacturerDiscount){
+            } else if (manafacturerDiscount > maxManafacturerDiscount) {
                 System.out.println("Our AutoShop needs to be profitable, please reconsider!");
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
-        }
-        catch(Exception e){
-            System.out.println("Check your input for invalid values");
+        } catch (Exception e) {
+            System.out.println("Invalid Input!!! Please provide valid inputs");
             return false;
         }
     }
 
-
 }
-
